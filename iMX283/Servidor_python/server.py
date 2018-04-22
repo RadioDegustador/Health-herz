f36cc620442c58cdd4e76259b60ddc3c63ec810f
LeoPerezLievano <clperezl@unal.edu.co>
2018-04-22 12:50:17 -0500

    Se suben las imágenes del kernel y el sistema de archivos, la PCB de adquisición de datos y la vesión inicial del servidor python.

---
+++ iMX283/Servidor_python/server.py
@@ -0,0 +1,24 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+#Librerias solo disponibles en python3
+
+HOST = 5050	#Puerto en el que estara el servidor
+
+class HttpHandler(BaseHTTPRequestHandler):
+
+
+	def do_GET(self):
+		self.send_response(200)
+		#Ahora se envian las cabeceras (estas son informacion del tipo de dato)
+		self.send_header('Content-type', 'text/HTML; charset=utf-8')
+		self.end_headers()
+
+		#Imprimo un fichero html que ya se tiene
+		message = open("formulario.html").read().encode()
+		self.wfile.write(message)
+
+if __name__ == "__main__":
+	server_address = ('', HOST) 				#Se declara la direccion del servidor
+	httpd = HTTPServer(server_address, HttpHandler)		#Se declara el servidor
+	print('Servidor abierto en el puerto: ');
+	print(HOST);
+	httpd.serve_forever()					#Se deja funcionando por siempre
