b28e5ca3c5b366a7dbbe62a39435501d4a9c5f1e
Niconzalez <negonzalezp@unal.edu.co>
2018-05-04 11:05:12 -0500

    se sube el blink de SAM3S, cambiar makefile para compilar con ChibiOS

---
+++ SAM3S/blink/.dep/pal_lld.o.d
@@ -0,0 +1,199 @@
+build/obj/pal_lld.o: \
+ /home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/hal/platforms/AT91SAM3S/pal_lld.c \
+ /home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/kernel/include/ch.h \
+ chconf.h \
+ /home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/ports/GCC/ARMCMx/chtypes.h \
+ /opt/gcc-arm-none-eabi-4_9-2015q3/lib/gcc/arm-none-eabi/4.9.3/include/stddef.h \
+ /opt/gcc-arm-none-eabi-4_9-2015q3/lib/gcc/arm-none-eabi/4.9.3/include/stdint.h \
+ /opt/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/stdint.h \
+ /opt/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/machine/_default_types.h \
+ /opt/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/sys/features.h \
+ /opt/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/sys/_intsup.h \
+ /opt/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/sys/_stdint.h \
+ /home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/kernel/include/chlists.h \
+ /home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/ports/GCC/ARMCMx/chcore.h \
+ /home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/ports/GCC/ARMCMx/AT91SAM3S/cmparams.h \
+ /home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/ports/GCC/ARMCMx/chcore_v7m.h \
+ /home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/ports/common/ARMCMx/nvic.h \
+ /home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/kernel/include/chsys.h \
+ /home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/kernel/include/chvt.h \
+ /home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/kernel/include/chschd.h \
+ /home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/kernel/include/chsem.h \
+ /home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/kernel/include/chbsem.h \
+ /home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/kernel/include/chmtx.h \
+ /home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/kernel/include/chcond.h \
+ /home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/kernel/include/chevents.h \
+ /home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/kernel/include/chmsg.h \
+ /home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/kernel/include/chmboxes.h \
+ /home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/kernel/include/chmemcore.h \
+ /home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/kernel/include/chheap.h \
+ /home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/kernel/include/chmempools.h \
+ /home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/kernel/include/chthreads.h \
+ /home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/kernel/include/chdynamic.h \
+ /home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/kernel/include/chregistry.h \
+ /home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/kernel/include/chinline.h \
+ /home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/kernel/include/chqueues.h \
+ /home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/kernel/include/chstreams.h \
+ /home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/kernel/include/chioch.h \
+ /home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/kernel/include/chfiles.h \
+ /home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/kernel/include/chdebug.h \
+ /home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/hal/include/hal.h \
+ /home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/boards/ATMEL_SAM3S_EK/board.h \
+ /home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/hal/platforms/AT91SAM3S/at91sam3.h \
+ /home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/hal/platforms/AT91SAM3S/at91lib/SAM3S.h \
+ /home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/hal/platforms/AT91SAM3S/core_cm3.h \
+ /home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/hal/platforms/AT91SAM3S/system_SAM3.h \
+ halconf.h mcuconf.h \
+ /home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/hal/platforms/AT91SAM3S/hal_lld.h \
+ /home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/hal/include/tm.h \
+ /home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/hal/include/pal.h \
+ /home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/hal/platforms/AT91SAM3S/pal_lld.h \
+ /home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/hal/include/adc.h \
+ /home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/hal/include/can.h \
+ /home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/hal/include/ext.h \
+ /home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/hal/include/gpt.h \
+ /home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/hal/include/i2c.h \
+ /home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/hal/include/icu.h \
+ /home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/hal/include/mac.h \
+ /home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/hal/include/pwm.h \
+ /home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/hal/include/rtc.h \
+ /home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/hal/include/serial.h \
+ /home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/hal/platforms/AT91SAM3S/serial_lld.h \
+ /home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/hal/include/sdc.h \
+ /home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/hal/include/spi.h \
+ /home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/hal/include/uart.h \
+ /home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/hal/include/usb.h \
+ /home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/hal/include/mmc_spi.h \
+ /home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/hal/include/serial_usb.h
+
+/home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/kernel/include/ch.h:
+
+chconf.h:
+
+/home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/ports/GCC/ARMCMx/chtypes.h:
+
+/opt/gcc-arm-none-eabi-4_9-2015q3/lib/gcc/arm-none-eabi/4.9.3/include/stddef.h:
+
+/opt/gcc-arm-none-eabi-4_9-2015q3/lib/gcc/arm-none-eabi/4.9.3/include/stdint.h:
+
+/opt/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/stdint.h:
+
+/opt/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/machine/_default_types.h:
+
+/opt/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/sys/features.h:
+
+/opt/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/sys/_intsup.h:
+
+/opt/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/sys/_stdint.h:
+
+/home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/kernel/include/chlists.h:
+
+/home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/ports/GCC/ARMCMx/chcore.h:
+
+/home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/ports/GCC/ARMCMx/AT91SAM3S/cmparams.h:
+
+/home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/ports/GCC/ARMCMx/chcore_v7m.h:
+
+/home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/ports/common/ARMCMx/nvic.h:
+
+/home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/kernel/include/chsys.h:
+
+/home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/kernel/include/chvt.h:
+
+/home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/kernel/include/chschd.h:
+
+/home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/kernel/include/chsem.h:
+
+/home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/kernel/include/chbsem.h:
+
+/home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/kernel/include/chmtx.h:
+
+/home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/kernel/include/chcond.h:
+
+/home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/kernel/include/chevents.h:
+
+/home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/kernel/include/chmsg.h:
+
+/home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/kernel/include/chmboxes.h:
+
+/home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/kernel/include/chmemcore.h:
+
+/home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/kernel/include/chheap.h:
+
+/home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/kernel/include/chmempools.h:
+
+/home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/kernel/include/chthreads.h:
+
+/home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/kernel/include/chdynamic.h:
+
+/home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/kernel/include/chregistry.h:
+
+/home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/kernel/include/chinline.h:
+
+/home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/kernel/include/chqueues.h:
+
+/home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/kernel/include/chstreams.h:
+
+/home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/kernel/include/chioch.h:
+
+/home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/kernel/include/chfiles.h:
+
+/home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/kernel/include/chdebug.h:
+
+/home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/hal/include/hal.h:
+
+/home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/boards/ATMEL_SAM3S_EK/board.h:
+
+/home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/hal/platforms/AT91SAM3S/at91sam3.h:
+
+/home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/hal/platforms/AT91SAM3S/at91lib/SAM3S.h:
+
+/home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/hal/platforms/AT91SAM3S/core_cm3.h:
+
+/home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/hal/platforms/AT91SAM3S/system_SAM3.h:
+
+halconf.h:
+
+mcuconf.h:
+
+/home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/hal/platforms/AT91SAM3S/hal_lld.h:
+
+/home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/hal/include/tm.h:
+
+/home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/hal/include/pal.h:
+
+/home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/hal/platforms/AT91SAM3S/pal_lld.h:
+
+/home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/hal/include/adc.h:
+
+/home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/hal/include/can.h:
+
+/home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/hal/include/ext.h:
+
+/home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/hal/include/gpt.h:
+
+/home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/hal/include/i2c.h:
+
+/home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/hal/include/icu.h:
+
+/home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/hal/include/mac.h:
+
+/home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/hal/include/pwm.h:
+
+/home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/hal/include/rtc.h:
+
+/home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/hal/include/serial.h:
+
+/home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/hal/platforms/AT91SAM3S/serial_lld.h:
+
+/home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/hal/include/sdc.h:
+
+/home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/hal/include/spi.h:
+
+/home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/hal/include/uart.h:
+
+/home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/hal/include/usb.h:
+
+/home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/hal/include/mmc_spi.h:
+
+/home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/hal/include/serial_usb.h:
---
+++ SAM3S/blink/.dep/pio.o.d
@@ -0,0 +1,26 @@
+build/obj/pio.o: \
+ /home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/hal/platforms/AT91SAM3S/pio.c \
+ /home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/hal/platforms/AT91SAM3S/pio.h \
+ /home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/hal/platforms/AT91SAM3S/at91lib/SAM3S.h \
+ /opt/gcc-arm-none-eabi-4_9-2015q3/lib/gcc/arm-none-eabi/4.9.3/include/stdint.h \
+ /opt/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/stdint.h \
+ /opt/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/machine/_default_types.h \
+ /opt/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/sys/features.h \
+ /opt/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/sys/_intsup.h \
+ /opt/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/sys/_stdint.h
+
+/home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/hal/platforms/AT91SAM3S/pio.h:
+
+/home/nicolas/Escritorio/Embebidos/stamp_repo/files/code/ext/ChibiOS/os/hal/platforms/AT91SAM3S/at91lib/SAM3S.h:
+
+/opt/gcc-arm-none-eabi-4_9-2015q3/lib/gcc/arm-none-eabi/4.9.3/include/stdint.h:
+
+/opt/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/stdint.h:
+
+/opt/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/machine/_default_types.h:
+
+/opt/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/sys/features.h:
+
+/opt/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/sys/_intsup.h:
+
+/opt/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/sys/_stdint.h:
