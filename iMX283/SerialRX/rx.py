cf7cf5542c3de8ed06c635784f4fb3fe41906beb
LeoPerezLievano <clperezl@unal.edu.co>
2018-05-17 08:58:50 -0500

    Serial RX funcionando en python(iMX283)

---
+++ iMX283/SerialRX/rx.py
@@ -0,0 +1,11 @@
+import serial
+#Libreria para usar el puerto serial
+
+#El puerto al que esta conectado el SAM3S es /dev/ttySP0
+SAM3S = serial.Serial('/dev/ttySP0',baudrate=115200,timeout = 3.0)
+
+while (1):
+	dato = SAM3S.readline()
+	print(dato)
+
+SAM3S.close()
