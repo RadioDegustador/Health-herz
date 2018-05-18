550901cc1abeb26ff9f6b1512346b513a3e3ea9e
ItsAPettiteGirl <keepcalmanddomaths@gmail.com>
2018-05-18 10:19:06 -0500

    Leo no me quiere ayudar

--- iMX283/Server/server.py
+++ iMX283/Server/server.py
@@ -4,11 +4,11 @@ app  = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('home.html')
+    return render_template('graph.html')
 
 @app.route('/graph', methods =['GET', 'POST'])
 def send():
-    return render_template ('graph.html')
+    return render_template('graph.html')
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
