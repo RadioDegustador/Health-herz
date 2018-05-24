54734d5ce9632446f4e3dd073e16d8ac9dab2cf7
ItsAPettiteGirl <keepcalmanddomaths@gmail.com>
2018-05-24 03:11:56 -0500

    server terminado

--- iMX283/SerialRX/rx.py
+++ iMX283/SerialRX/rx.py
@@ -4,8 +4,9 @@ import serial
 #El puerto al que esta conectado el SAM3S es /dev/ttySP0
 SAM3S = serial.Serial('/dev/ttySP0',baudrate=115200,timeout = 3.0)
 
+
 while (1):
 	dato = SAM3S.readline()
 	print(dato)
 
-SAM3S.close()
+#SAM3S.close()
