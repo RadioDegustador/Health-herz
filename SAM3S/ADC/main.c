00829add0a9eb85173dcc95717e6823140dc8340
Niconzalez <negonzalezp@unal.edu.co>
2018-05-30 15:51:55 -0500

    Filtro en digital variables de estado, implementado en SAM3S

--- SAM3S/ADC/main.c
+++ SAM3S/ADC/main.c
@@ -29,37 +29,11 @@ static msg_t Thread1(void *arg) {
 }
 
 
-//Kernel es la respuesta al impulso del filtro
-/*void convolve(const float Signal[/* SignalLen */], size_t SignalLen,
-//              const float Kernel[/* KernelLen */], size_t KernelLen,
-//              float Result[/* SignalLen + KernelLen - 1 */])
-/*{
-  size_t n;
-
-  for (n = 0; n < SignalLen + KernelLen - 1; n++)
-  {
-    size_t kmin, kmax, k;
-
-    Result[n] = 0;
-
-    kmin = (n >= KernelLen - 1) ? n - (KernelLen - 1) : 0;
-    kmax = (n < SignalLen - 1) ? n : SignalLen - 1;
-    for (k = kmin; k <= kmax; k++)
-    {
-      Result[n] += Signal[k] * Kernel[n - k];
-    }
-  }
-}*/
-
-/*h es la respuesta al impulso del filtro con fs = 250Hz*/
-/*float h [] = {-0.0593341497149129 ,-0.131643055995946, -0.132008742782508, -0.00183627497553400, 0.187348576956375, 0.278006552946558, 0.187348576956375, -0.00183627497553400, -0.132008742782508, -0.131643055995946, -0.0593341497149129};*/
-
-
 /*
  * Application entry point.
  */
 int main(void) {
-   int ADC_Val[200]; //tiene que ser un entero
+   float ADC_Val; //tiene que ser un entero
 
    halInit();
    chSysInit();
@@ -98,37 +72,40 @@ int main(void) {
 
    while (TRUE) {
     while( !(ADC->ADC_ISR & ADC_ISR_EOC0));//Este While revisa que no halla una interrupción, de existir vuelve a iniciar el ciclo While(TRUE)
-    for (int i= 0;i<200; i ++){
+
     	chThdSleepMilliseconds(500);  /*cada 500 milisegundos hago el procedimiento de tomar todos los valores ADC_DCR y alojarlos en cada //
     	espacio de ADC_Val[]*/    
-      	ADC_Val[i] = ADC->ADC_CDR[0]; //ADC_CDR registro que lee el ADC, existen hasta 14 
+      	ADC_Val = ADC->ADC_CDR[0]; //ADC_CDR registro que lee el ADC, existen hasta 14 
     	//chprintf((BaseChannel *)&SD2, "%d \r\n", ADC_Val[i]*3300/4096);
     	
     	float x1=0, x2= 0,x3=0 ,x4= 0,x5=0,x6=0,x7=0,x8=0,x9=0,x10=0,y=0;
     	
-    	x1  = ADC_Val[i];
-    	x2  = x1;
-    	x3  = x2;
-    	x4  = x3;
-    	x5  = x4;
-    	x6  = x5;
-    	x7  = x6;
-    	x8  = x9;
-    	x9  = x8;
-    	x10 = x9;
     	
-    	y = -0.131643055995946*x1-0.132008742782508*x2-0.001836274975534*x3-0.187348576956375*x4+0.278006552946558*x5+0.187348576956375*x6-0.001836274975534*x7-0.132008742782508*x8-0.131643055995946*x9-0.059334149714913*x10;
+    	y = -0.131643055995946*x1-0.132008742782508*x2-0.001836274975534*x3-0.187348576956375*x4+0.278006552946558*x5+0.187348576956375*x6-0.001836274975534*x7-0.132008742782508*x8-0.131643055995946*x9-0.059334149714913*x10-0.059334149714913*ADC_Val;
+    	
+    	x10 = x9;
+    	x9  = x8;
+    	x8  = x9;
+    	x7  = x6;
+    	x6  = x5;
+    	x5  = x4;
+    	x4  = x3;
+    	x3  = x2;
+    	x2  = x1;
+    	x1  = ADC_Val;
+    	
+    	
     		   
     		   
     	chprintf((BaseChannel *)&SD2, "%d \r\n",y*3300/4096);	   	 
-    };
+    
    
     
     //	chThdSleepMilliseconds(1000);
     //float  salida[ELEMENT_COUNT(ADC_Val)+ELEMENT_COUNT(h)-1];
    
     //convolve(ADC_Val,ELEMENT_COUNT(ADC_Val),h,ELEMENT_COUNT(h),salida);// Si no sirve comentar esa línea
-   }
+   };
    
    
    return(0);
